c34ba129c8a0a9749d26455a9436d06f2afbf8bb
atmaniali <atmaniali97@gmail.com>
2023-08-15 18:52:10 +0100

    i fix some bugs and add data t my table

--- book/models.py
+++ book/models.py
@@ -8,23 +8,23 @@ from django.template.defaultfilters import slugify
 class Book(models.Model):
     title = models.CharField(max_length=300, blank=True, null=True)
     slug = models.SlugField(max_length=300)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='author_book')
+    author = models.CharField(max_length=200, blank=True, null=True)
     genre = models.CharField(max_length=150, blank=True, null=True)
-    rating = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rating = models.FloatField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Book, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.title} wrote by {self.author.username}"
+        return f"{self.title} wrote by {self.author}"
 
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_review')
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='book_review')
-    content = models.TextField(blank=True, null=True)
-    rating = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    review_text = models.TextField(blank=True, null=True)
+    rating = models.FloatField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def __str__(self):
         return f"{self.user.username} review {self.book.title}"
--- book/serializer.py
+++ book/serializer.py
@@ -6,11 +6,11 @@ from .models import Book, Review
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        field = ['url', 'id', 'author', 'slug', 'title', 'genre', 'rating']
+        fields = ['url', 'id', 'author', 'slug', 'title', 'genre', 'rating']
         read_only_fields = ['slug']
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        field = Review
-        field = ['url', 'id', 'user', 'book', 'content', 'rating']
+        model = Review
+        fields = ['url', 'id', 'user', 'book', 'review_text', 'rating']
