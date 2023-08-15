387c247e93715b4686b78e485380fcad2b3220ec
atmaniali <atmaniali97@gmail.com>
2023-08-15 22:43:49 +0100

    test token authentication

--- users/viewsets.py
+++ users/viewsets.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .serializer import UserSerializer
 from .permission import IsUserOwnerORGetOrPost
 
@@ -8,4 +9,6 @@ from .permission import IsUserOwnerORGetOrPost
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
     permission_classes = [IsUserOwnerORGetOrPost]
